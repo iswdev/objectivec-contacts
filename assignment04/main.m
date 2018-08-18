93d49e0b384cf9c7d592d78f41914843a580d91f
Francisco Igor <franciscoigor@gmail.com>
2018-08-17 17:55:40 -0700

    Initial structure

---
+++ assignment04/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  assignment04
+//
+//  Created by Francisco on 2018-08-17.
+//  Copyright © 2018 project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        
+        
+    }
+    return 0;
+}
